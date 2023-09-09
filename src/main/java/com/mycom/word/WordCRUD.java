10c24b2925717d3096fdf951097256066c3b7920
s1mjane <s1mjane@handong.ac.kr>
2023-09-09 18:29:47 +0900

    Update Item & Delete Item

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -64,4 +64,50 @@ public class WordCRUD implements ICRUD {
         }
         System.out.println("---------------------------------\n\n");
     }
+
+    public ArrayList<Integer> listAll(String keyword) {
+        ArrayList<Integer> idlist = new ArrayList<>();
+        int j=0;
+        System.out.println("---------------------------------");
+        for (int i = 0; i < list.size(); i++) {
+            String word = list.get(i).getWord();
+            if(!word.contains(keyword)) continue;
+            System.out.print((j+1) + " ");
+            System.out.println(list.get(i).toString());
+            idlist.add(i);
+            j++;
+        }
+        System.out.println("---------------------------------");
+        return idlist;
+    }
+
+    public void updateItem() {
+        System.out.print("=> 수정할 단어 검색 : ");
+        String keyword = s.next(); // next: 공백을 허용하지 않기 위해서
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 수정할 번호 검색 : ");
+        int id = s.nextInt();
+        s.nextLine();
+        System.out.print("=> 뜻 입력 : ");
+        String meaning = s.nextLine(); // nextLine: 공백 포함해서 입력 받기
+        Word word = list.get(idlist.get(id-1));
+        word.setMeaning(meaning);
+        System.out.println("단어가 수정되었습니다.");
+    }
+
+    public void deleteItem() {
+        System.out.print("=> 삭제할 단어 검색 : ");
+        String keyword = s.next(); // next: 공백을 허용하지 않기 위해서
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 수정할 번호 검색 : ");
+        int id = s.nextInt();
+        s.nextLine();
+        System.out.print("=> 정말로 삭제하시겠습니까?(Y/N) : ");
+        String ans = s.next();
+        if(ans.equalsIgnoreCase("y")) {
+            list.remove((int)idlist.get(id-1));
+            System.out.println("단어가 삭제되었습니다.");
+        } else
+            System.out.println("취소되었습니다.");
+    }
 }
--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -36,6 +36,12 @@ public class WordManager {
             else if(menu == 4) {
                 wordCRUD.addItem();
             }
+            else if(menu == 5) {
+                wordCRUD.updateItem();
+            }
+            else if(menu == 6) {
+                wordCRUD.deleteItem();
+            }
         }
     }
 }
